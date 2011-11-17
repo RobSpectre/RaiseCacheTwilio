dec16031d85993b0748a158d56345eaea84bd67d
Jared Wyatt <j@wyatt.co>
2011-11-17 17:08:21 -0500

    fixed auction test data

--- test/auction_seeder.rb
+++ test/auction_seeder.rb
@@ -11,7 +11,7 @@ db = Mongo::Connection.new.db(db_name)
 (1..15).each do |i|
   db[items_coll].insert({
     'number' => i,
-    'name' => 'Item #{i}',
-    'info' => 'This is the info for auction item #{i}.'
+    'name' => "Item #{i}",
+    'info' => "This is the info for auction item #{i}."
   })
 end
